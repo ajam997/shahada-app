1e419d76de4b0a28d27d3ff55f3cadd33849d993
ajam <ajamm1997@gmail.com>
2024-08-17 17:52:13 +0300

    add animation

--- src/services/SpeechService.js
+++ src/services/SpeechService.js
@@ -1,6 +1,5 @@
-export const speak = (text, lang) => {
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = lang;
-    window.speechSynthesis.speak(utterance);
-  };
-  
+export const speak = (text, lang = 'en-US') => {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = lang;
+  window.speechSynthesis.speak(utterance);
+};
